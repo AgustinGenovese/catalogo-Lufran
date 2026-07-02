6519036e00b1b80d28ddce02fbbad589ca8238d1
Agustin Genovese <agusgenot@gmail.com>
2026-07-01 23:40:33 -0300

    feat: preparar deploy en Vercel (app.js + api/) y fix typo en app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
+
+const pagesRoutes = require("./routes/pagesRoutes");
+const productoRoutes = require("./routes/productoRoutes");
+
+const conectarDB = require("./config/db");
+
+conectarDB();
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use("/api", productoRoutes);
+app.use("/", pagesRoutes);
+
+app.use((req, res) => {
+  res.status(404).send("Página no encontrada");
+});
+
+module.exports = app;
